f042a5512a3476296a692008899faf4f9c291311
Kyle Martin <kyle.martin@tharsus.com>
2020-12-11 09:47:51 +0000

    add scripts

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    public string plantName;
+    public int plantHealthRestoreValue;
+    public Sprite plantSprite;
+    public float plantGrowthTime;
+    public int plantCurrentGrowState = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
